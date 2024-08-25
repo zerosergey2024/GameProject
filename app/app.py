0d4bdcb54c461c1298b0a1054a2fd95ed2ce6c6d
statelinecat <statelinecat@gmail.com>
2024-08-25 17:58:07 +0300

    add code

--- app/app.py
+++ app/app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return render_template('home.html')
 
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
